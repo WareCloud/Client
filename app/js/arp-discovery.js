2880fe077d6e4c52be8354960d420e8595e71b1c
Nicolas Boufidjeline <nico.bouf.45275@gmail.com>
2018-03-28 01:20:25 +0200

    windows network discovery

--- app/js/arp-discovery.js
+++ app/js/arp-discovery.js
@@ -7,30 +7,48 @@ function print(array){
   }
   return ret;
 }
+var output = [];
 
-fs.readFile('/proc/net/arp', function(err, data) {
-  if (!!err) return done(err, null);
-
-  var output = [];
-  var tableau = [];
-  var devices = data.toString().split('\n');
-  devices.splice(0,1);
-
-  for (i = 0; i < devices.length; i++) {
-    var cols = devices[i].replace(/ [ ]*/g, ' ').split(' ');
-
-    if ((cols.length > 3) && (cols[0].length !== 0) && (cols[3].length !== 0) && cols[3] !== '00:00:00:00:00:00') {
-      if (tableau.lenght == 0)
-        tableau.push(cols[0]);
-      else
-        tableau.push("<br />" + cols[0]);
-      output.push({
-          ip: cols[0],
-          mac: cols[3]
-        });
-      }
+if (process.platform === 'win32'){
+  const { exec } = require('child_process');
+  exec('arp -a', (err, stdout, stderr) => {
+    if (err) {
+      // node couldn't execute the command
+      return;
     }
+    var devices = stdout.toString().split('\n');
+    var tableau = [];
+    for (var i in devices){
+      if (devices[i].indexOf("dynamique") != -1){
+        var cols = devices[i].replace(/ [ ]*/g, ' ').split(' ');
+          output.push({
+            ip: cols[1],
+            mac: cols[2]
+          })
+        }
+      }
+    document.getElementById("deviceTable").innerHTML = print(output);
+  });
+}
+else {
+  fs.readFile('/proc/net/arp', function(err, data) {
+    if (!!err) return done(err, null);
 
-  console.log(output);
-  document.getElementById("deviceTable").innerHTML = print(output);
-});
+    var tableau = [];
+    var devices = data.toString().split('\n');
+    devices.splice(0,1);
+
+    for (i = 0; i < devices.length; i++) {
+      var cols = devices[i].replace(/ [ ]*/g, ' ').split(' ');
+
+      if ((cols.length > 3) && (cols[0].length !== 0) && (cols[3].length !== 0) && cols[3] !== '00:00:00:00:00:00') {
+        output.push({
+            ip: cols[0],
+            mac: cols[3]
+          });
+        }
+      }
+      document.getElementById("deviceTable").innerHTML = print(output);
+  });
+}
+console.log(output);
